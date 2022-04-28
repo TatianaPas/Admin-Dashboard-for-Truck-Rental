994f645cead44d62e300526fe9f066d0862c447f
Tatiana <tatjana.pasechnik@gmail.com>
2022-04-28 17:48:25 +1200

    create login page

---
+++ NewDesignTrial/View/AdminDashboard.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NewDesignTrial.View
+{
+    /// <summary>
+    /// Interaction logic for AdminDashboard.xaml
+    /// </summary>
+    public partial class AdminDashboard : Window
+    {
+        public AdminDashboard()
+        {
+            InitializeComponent();
+        }
+        private void buttonMinimise_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void WindowStateBtnClick(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void CloseBtnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
+
+   
